b4882788b622a64156116c694288e861b790f7ea
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-07-16 00:58:17 +0100

    AI Chat OG

---
+++ Chat-OG93/src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return (
+    <>
+      <div className='w-full mx-auto bg-slate-500'>
+        <h1>Chat OG93</h1>
+       <input
+       className='w-[500px]'
+       type='text'
+       placeholder='Ask Me anything...'/>
+      </div>
+    </>
+  )
+}
+
+export default App
