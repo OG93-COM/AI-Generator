4bf4f2876cbc8bfbcb2ec6b3c635a71bf7572335
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-07-19 02:01:33 +0100

    AI Chat

--- ChatOG93/src/components/Message.jsx
+++ ChatOG93/src/components/Message.jsx
@@ -1,16 +1,12 @@
 import { useSelector } from "react-redux"
 import ReactMarkdown from 'react-markdown'
 
-
-
 const Message = ({imgProfile, message}) => {
-
     const messageText = useSelector(state => state.chatAi)
     console.log(messageText)
-    
   return (
     <div className='flex items-start gap-2 my-2  '>
-        <img src={imgProfile} className='w-14 px-1 mb-2'/>
+        <img src={imgProfile} className='w-12 px-1 mb-2'/>
         <ReactMarkdown className={imgProfile === "/src/assets/User-Profile.png" ? 'bg-blue-600 p-2 rounded-lg' : 'bg-slate-600 p-2 rounded-lg '}>
        {message}
         </ReactMarkdown>
