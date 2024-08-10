1a8c00e6184a975e9787ae450b7e1597e6fd7633
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-08-11 00:17:39 +0100

    FaceIA

--- ChatOG93/src/components/Header.jsx
+++ ChatOG93/src/components/Header.jsx
@@ -3,18 +3,17 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div  className=' z-auto w-full h-14 bg-slate-700 flex justify-center items-center text-slate-100 font-bold gap-10'>
-      <Link  to="/">
-      <div className='cursor-pointer hover:text-slate-300'>Chat AI</div>
+    <div className=' z-auto w-full h-14 bg-slate-700 flex justify-center items-center text-slate-100 font-bold gap-10'>
+      <Link to="/">
+        <div className='cursor-pointer hover:text-slate-300'>Chat AI</div>
       </Link>
-      <Link  to="/GenAI-Img">
-      <div className='cursor-pointer hover:text-slate-300'>Generative Image</div>
+      <Link to="/GenAI-Img">
+        <div className='cursor-pointer hover:text-slate-300'>Generative Image</div>
       </Link>
-      <Link  to="/Face-AI">
-      <div className='cursor-pointer hover:text-slate-300'>Face AI</div>
+      <Link to="/Face-AI">
+        <div className='cursor-pointer hover:text-slate-300'>Face AI</div>
       </Link>
     </div>
-    
   )
 }
 
